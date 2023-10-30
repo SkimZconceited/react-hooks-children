feb09b9c385d263bdb5093ea8480c913d6c08146
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-30 11:43:35 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,22 @@ function App() {
   return (
     <div className="App">
       <Container header="Example header" direction="horizontal">
+        {/* <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
         <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+        <div>This is an example!</div> */}
+
+        <ExampleElement message='This is an example!' />
+        <ExampleElement message='This is an example!' />
+        <ExampleElement message='This is an example!' />
+        <ExampleElement message='This is an example!' />
+        <ExampleElement message='This is an example!' />
       </Container>
       <Container header="Example header" textPosition="center">
-        <img
+        {/* <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
         />
@@ -29,10 +35,23 @@ function App() {
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+        /> */}
+
+        <ExampleImg alt={'turtle'} src={'https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg'} />
+        <ExampleImg alt={'turtle'} src={'https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg'} />
+        <ExampleImg alt={'turtle'} src={'https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg'} />
+        <ExampleImg alt={'turtle'} src={'https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg'} />
       </Container>
     </div>
   );
+}
+
+function ExampleElement({message}) {
+  return <>{message}</>
+}
+
+function ExampleImg({alt, src}) {
+  return <img alt={alt} src={src}></img>
 }
 
 export default App;
